9b29028f21f6417965e3c03cbc49f1b593551c20
5ym <16yuim@gmail.com>
2022-10-12 12:15:32 +0900

    fix pyenv

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ with open("/code/config.example.yaml", "r") as yf:
     data['general']['mirakurun_url'] = 'http://mirakurun:40772/'
     data['general']['encoder'] = 'FFmpeg'
     data['capture']['upload_folder'] = '/capture'
-    print(data)
     with open('/code/config.yaml', 'w') as file:
         yaml.dump(data, file)
--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ cd docker-mirakurun-konomitv
 cp docker-compose.example.yml docker-compose.yml
 docker compose run --rm -e SETUP=true mirakurun
 touch konomitv/config.yaml
-docker compose run --rm -v $PWD/setup.py:/code/setup.py --entrypoint sh konomitv -c 'thirdparty/Python/bin/pip install pyyaml && thirdparty/Python/bin/python ../setup.py'
+docker compose run --rm -v $PWD/setup.py:/code/setup.py --entrypoint sh konomitv -c '.venv/bin/python ../setup.py'
