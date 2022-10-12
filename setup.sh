d93f521a8208ddb31adddcc95a269d021f9a5a53
5ym <16yuim@gmail.com>
2022-10-12 11:55:33 +0900

    feature reference original image

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/5ym/docker-mirakurun-konomitv.git
 cd docker-mirakurun-konomitv
 cp docker-compose.example.yml docker-compose.yml
-cp konomitv/config.example.yaml konomitv/config.yaml
 docker compose run --rm -e SETUP=true mirakurun
+touch konomitv/config.yaml
+docker compose run --rm -v $PWD/setup.py:/code/setup.py --entrypoint sh konomitv -c 'thirdparty/Python/bin/pip install pyyaml && thirdparty/Python/bin/python ../setup.py'
