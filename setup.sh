e4ac70919748d0867708cccbeb457b2695727551
Ruk <16yuim@gmail.com>
2023-09-17 14:24:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/5ym/docker-mirakurun-konomitv.git
 cd docker-mirakurun-konomitv
-cp docker-compose.example.yml docker-compose.yml
+cp compose.example.yml compose.yml
 docker compose run --rm -e SETUP=true mirakurun
 touch konomitv/config.yaml
 docker compose run --rm -v $PWD/setup.py:/code/setup.py --entrypoint sh konomitv -c '.venv/bin/python ../setup.py'
